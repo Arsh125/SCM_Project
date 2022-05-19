9a18cbfd86ea94d5fb177ccb49eb9ea32bd8fd99
Arsh <arshranga5@gmail.com>
2022-05-19 23:49:46 +0530

    Scm project

---
+++ Function1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter your fav. number"<<endl;
+    cin>>a;
+    cout<<"your fav. number is"<<a;
+    return 0;
+}
---
+++ FunctionPrototype.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void add(int, int);
+int main(){
+    add(6,3);
+    void add(int a, int b){
+        cout<<(a+b);
+    }
+    return 0;
+}
